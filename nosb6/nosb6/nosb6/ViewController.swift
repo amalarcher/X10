bb9eedd919782bfa1b6d533fe6273958a9e8dcbe
amalarcher <andre.malarcher@softwaremotifs.com>
2018-10-14 16:58:47 -0400

    commit at 4:58pm

---
+++ nosb6/nosb6/nosb6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nosb6
+//
+//  Created by ANDRE MALARCHER on 10/14/18.
+//  Copyright © 2018 Software Motifs LLC. All rights reserved.
+//1
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
